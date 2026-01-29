76df37fe98eba2301692d8f5740952a9ce5b1e9e
MrTarik2112 <mr.tarik2112@gmail.com>
2026-01-29 16:36:14 +0300

    Update: Added real hardware drivers and fixed games

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -4,7 +4,6 @@
  */
 
 /* ============= HEADERS ============= */
-// Minimal definitions to avoid external dependencies
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
@@ -13,6 +12,20 @@ typedef int bool;
 #define true 1
 #define false 0
 #define NULL ((void *)0)
+
+// Multiboot header
+typedef struct multiboot_info {
+  uint32_t flags;
+  uint32_t mem_lower;
+  uint32_t mem_upper;
+  uint32_t boot_device;
+  uint32_t cmdline;
+  uint32_t mods_count;
+  uint32_t mods_addr;
+  uint32_t syms[4];
+  uint32_t mmap_length;
+  uint32_t mmap_addr;
+} multiboot_info_t;
 
 /* ============= VGA DRIVER ============= */
 #define VGA_MEMORY 0xB8000
@@ -140,13 +153,6 @@ void *memset(void *s, int c, int n) {
     *p++ = (unsigned char)c;
   return s;
 }
-void *memcpy(void *d, const void *s, int n) {
-  char *dp = d;
-  const char *sp = s;
-  while (n--)
-    *dp++ = *sp++;
-  return d;
-}
 
 int atoi(const char *s) {
   int n = 0, neg = 0;
@@ -185,13 +191,7 @@ void print_int(int n) {
   print(buf);
 }
 
-static uint32_t rand_seed = 123456789;
-uint32_t rand(void) {
-  return (rand_seed = rand_seed * 1103515245 + 12345) & 0x7FFFFFFF;
-}
-void srand(uint32_t seed) { rand_seed = seed; }
-
-/* ============= I/O & TIMER ============= */
+/* ============= I/O PORTS ============= */
 static inline uint8_t inb(uint16_t port) {
   uint8_t r;
   __asm__ volatile("inb %1, %0" : "=a"(r) : "Nd"(port));
@@ -201,11 +201,59 @@ static inline void outb(uint16_t port, uint8_t v) {
   __asm__ volatile("outb %0, %1" : : "a"(v), "Nd"(port));
 }
 
+/* ============= TIMER & RTC ============= */
 volatile uint32_t ticks = 0;
 void sleep(uint32_t ms) {
-  uint32_t e = ticks + ms / 10;
+  uint32_t e = ticks + ms / 18;
   while (ticks < e)
     __asm__ volatile("hlt");
+}
+
+// RTC (Real Time Clock)
+#define CMOS_ADDR 0x70
+#define CMOS_DATA 0x71
+
+uint8_t get_rtc(int reg) {
+  outb(CMOS_ADDR, reg);
+  return inb(CMOS_DATA);
+}
+
+void get_time(int *h, int *m, int *s, int *d, int *mo, int *y) {
+  uint8_t statusB = get_rtc(0x0B);
+  bool bcd = !(statusB & 0x04);
+
+  *s = get_rtc(0x00);
+  *m = get_rtc(0x02);
+  *h = get_rtc(0x04);
+  *d = get_rtc(0x07);
+  *mo = get_rtc(0x08);
+  *y = get_rtc(0x09);
+
+  if (bcd) {
+    *s = ((*s & 0xF0) >> 1) + ((*s & 0xF0) >> 3) + (*s & 0x0F);
+    *m = ((*m & 0xF0) >> 1) + ((*m & 0xF0) >> 3) + (*m & 0x0F);
+    *h = ((*h & 0xF0) >> 1) + ((*h & 0xF0) >> 3) + (*h & 0x0F);
+    *d = ((*d & 0xF0) >> 1) + ((*d & 0xF0) >> 3) + (*d & 0x0F);
+    *mo = ((*mo & 0xF0) >> 1) + ((*mo & 0xF0) >> 3) + (*mo & 0x0F);
+    *y = ((*y & 0xF0) >> 1) + ((*y & 0xF0) >> 3) + (*y & 0x0F);
+  }
+  *y += 2000;
+}
+
+/* ============= HARDWARE INFO ============= */
+void get_cpu_brand(char *buffer) {
+  // Basic CPUID check for Vendor ID
+  uint32_t ebx, ecx, edx;
+  __asm__ volatile("cpuid" : "=b"(ebx), "=c"(ecx), "=d"(edx) : "a"(0));
+  *((uint32_t *)buffer) = ebx;
+  *((uint32_t *)(buffer + 4)) = edx;
+  *((uint32_t *)(buffer + 8)) = ecx;
+  buffer[12] = 0;
+}
+
+uint32_t rand_seed = 12345;
+uint32_t rand(void) {
+  return (rand_seed = rand_seed * 1103515245 + 12345) & 0x7FFFFFFF;
 }
 
 /* ============= KEYBOARD ============= */
@@ -223,9 +271,26 @@ char get_key(void) {
       'J', 'K', 'L',  ':',  '"',  '~', 0,   '|', 'Z', 'X', 'C', 'V',
       'B', 'N', 'M',  '<',  '>',  '?', 0,   '*', 0,   ' '};
   static bool shift = false;
+
   while (1) {
+    // RTC Clock in Status bar while waiting for key
+    if ((ticks % 18) == 0) {
+      int h, m, s, d, mo, y;
+      get_time(&h, &m, &s, &d, &mo, &y);
+      h = (h + 3) % 24; // Timezone +3
+      char timebuf[10];
+      timebuf[0] = h / 10 + '0';
+      timebuf[1] = h % 10 + '0';
+      timebuf[2] = ':';
+      timebuf[3] = m / 10 + '0';
+      timebuf[4] = m % 10 + '0';
+      timebuf[5] = 0;
+      print_at(70, 24, timebuf, WHITE, BLACK);
+    }
+
     if (inb(0x64) & 1) {
       uint8_t sc = inb(0x60);
+      ticks++; // Increment tick on input too due to lack of timer ISR yet
       if (sc == 0x2A || sc == 0x36)
         shift = true;
       else if (sc == 0xAA || sc == 0xB6)
@@ -233,16 +298,18 @@ char get_key(void) {
       else if (!(sc & 0x80) && sc < 58)
         return shift ? upper[sc] : lower[sc];
     }
-    ticks++; // Keep timer running
+    // Minimal delay
+    for (volatile int i = 0; i < 1000; i++)
+      ;
+    ticks++;
   }
 }
 
-// Non-blocking key check
+// Non-blocking key check for games
 char check_key(void) {
   if (inb(0x64) & 1) {
     uint8_t sc = inb(0x60);
     if (!(sc & 0x80)) {
-      // Simplified for games (WASD)
       const char map[] = {0,    27,  '1', '2',  '3',  '4',  '5', '6', '7',  '8',
                           '9',  '0', '-', '=',  '\b', '\t', 'q', 'w', 'e',  'r',
                           't',  'y', 'u', 'i',  'o',  'p',  '[', ']', '\n', 0,
@@ -269,7 +336,7 @@ typedef struct {
   bool used;
 } inode_t;
 inode_t fs[MAX_FILES];
-int current_dir = 0; // 0 is root
+int current_dir = 0;
 
 void fs_init() {
   memset(fs, 0, sizeof(fs));
@@ -278,12 +345,10 @@ void fs_init() {
   fs[0].used = true;
   fs[0].parent_id = 0;
 
-  // Default files
   int id = 1;
   strcpy(fs[1].name, "readme.txt");
   strcpy(fs[1].data,
-         "Welcome to TarkOS v1.0!\nThis is a RAM Filesystem.\n\n50+ Commands "
-         "available!\nTry 'help' or 'snake'.");
+         "TarkOS v1.0\nReal Hardware Mode.\nTry 'snake', 'info', 'date'.");
   fs[1].size = strlen(fs[1].data);
   fs[1].used = true;
   fs[1].parent_id = 0;
@@ -311,32 +376,31 @@ int fs_create(const char *name, bool is_dir) {
       return i;
     }
   }
-  return -2; // Full
+  return -2;
 }
 
-/* ============= SHELL & COMMANDS ============= */
+/* ============= SHELL COMMANDS ============= */
 void cmd_ls() {
   print("\nListing directory:\n");
   int cnt = 0;
   for (int i = 0; i < MAX_FILES; i++) {
     if (fs[i].used && fs[i].parent_id == current_dir && i != 0) {
-      if (fs[i].is_dir)
-        print_color(fs[i].name, LIGHT_BLUE);
-      else
-        print_color(fs[i].name, WHITE);
-      print(fs[i].is_dir ? "/\n" : "\n");
+      print(fs[i].is_dir ? "[" : "");
+      print_color(fs[i].name, fs[i].is_dir ? LIGHT_BLUE : WHITE);
+      print(fs[i].is_dir ? "]" : "");
+      print("\n");
       cnt++;
     }
   }
   print("Total: ");
   print_int(cnt);
-  print("\n");
+  print(" files\n");
 }
 
 void cmd_cat(char *arg) {
   int id = fs_find(arg);
   if (id == -1 || fs[id].is_dir)
-    print("\nFile not found or is directory.\n");
+    print("\nError: File not found.\n");
   else {
     print("\n");
     print(fs[id].data);
@@ -344,30 +408,48 @@ void cmd_cat(char *arg) {
   }
 }
 
-void cmd_touch(char *arg) {
-  if (fs_create(arg, false) == -2)
-    print("\nDisk full!\n");
-  else
-    print("\nFile created.\n");
+void cmd_info(multiboot_info_t *mboot) {
+  set_color(LIGHT_CYAN, BLUE);
+  print("\n=== System Information ===\n");
+  set_color(WHITE, BLUE);
+  print("  OS:       TarkOS v1.0\n");
+
+  char cpu[13];
+  get_cpu_brand(cpu);
+  print("  CPU:      ");
+  print(cpu);
+  print("\n");
+
+  int mem = 0;
+  if (mboot->flags & 1)
+    mem = (mboot->mem_lower + mboot->mem_upper) / 1024;
+  print("  RAM:      ");
+  print_int(mem);
+  print(" MB\n");
+  print("  Uptime:   ");
+  print_int(ticks / 18);
+  print(" sec\n");
 }
 
-void cmd_mkdir(char *arg) {
-  if (fs_create(arg, true) == -2)
-    print("\nDisk full!\n");
-  else
-    print("\nDirectory created.\n");
+void cmd_date() {
+  int h, m, s, d, mo, y;
+  get_time(&h, &m, &s, &d, &mo, &y);
+  h = (h + 3) % 24;
+  print("\nDate: ");
+  print_int(d);
+  print("/");
+  print_int(mo);
+  print("/");
+  print_int(y);
+  print("\nTime: ");
+  print_int(h);
+  print(":");
+  print_int(m);
+  print(":");
+  print_int(s);
+  print("\n");
 }
 
-void cmd_rm(char *arg) {
-  int id = fs_find(arg);
-  if (id != -1) {
-    fs[id].used = false;
-    print("\nDeleted.\n");
-  } else
-    print("\nNot found.\n");
-}
-
-/* ============= GAMES ============= */
 void game_snake() {
   struct {
     int x, y;
@@ -390,33 +472,32 @@ void game_snake() {
       put_char_at(39, y, '#', WHITE, BLACK);
     }
 
-    print_at(2, 0, "SNAKE - WASD to Move, Q to Quit", YELLOW, BLACK);
+    print_at(2, 0, "Score: ", YELLOW, BLACK);
+    print_int(score);
     put_char_at(fx, fy, '*', RED, BLACK);
 
     for (int i = 0; i < len; i++)
-      put_char_at(snake[i].x, snake[i].y, i == 0 ? '@' : 'o', GREEN, BLACK);
+      put_char_at(snake[i].x, snake[i].y, 'O', GREEN, BLACK);
 
-    if (check_key()) {
-      char c = get_key();
-      if (c == 'w' && dy == 0) {
-        dx = 0;
-        dy = -1;
-      }
-      if (c == 's' && dy == 0) {
-        dx = 0;
-        dy = 1;
-      }
-      if (c == 'a' && dx == 0) {
-        dx = -1;
-        dy = 0;
-      }
-      if (c == 'd' && dx == 0) {
-        dx = 1;
-        dy = 0;
-      }
-      if (c == 'q')
-        break;
+    char c = check_key();
+    if (c == 'w' && dy == 0) {
+      dx = 0;
+      dy = -1;
     }
+    if (c == 's' && dy == 0) {
+      dx = 0;
+      dy = 1;
+    }
+    if (c == 'a' && dx == 0) {
+      dx = -1;
+      dy = 0;
+    }
+    if (c == 'd' && dx == 0) {
+      dx = 1;
+      dy = 0;
+    }
+    if (c == 'q')
+      break;
 
     // Move
     put_char_at(snake[len - 1].x, snake[len - 1].y, ' ', BLACK, BLACK);
@@ -440,7 +521,7 @@ void game_snake() {
       fx = (rand() % 38) + 1;
       fy = (rand() % 18) + 2;
     }
-    sleep(100);
+    sleep(150);
   }
 end:
   clear_screen();
@@ -449,77 +530,11 @@ end:
   print("\n");
 }
 
-void game_tetris() {
-  print("\nTetris placeholder (simulated):\n");
-  print("[][][]\n  []  \n\nScore: 100\n");
-}
+/* ============= KERNEL MAIN ============= */
+void kmain(uint32_t magic, multiboot_info_t *mboot) {
+  // Enable Interrupts explicitly
+  __asm__ volatile("sti");
 
-void game_hangman() {
-  const char *word = "KERNEL";
-  char guess[10] = "______";
-  int tries = 6;
-  print("\nHANGMAN\n");
-  while (tries > 0 && strcmp(word, guess) != 0) {
-    print("\nWord: ");
-    print(guess);
-    print("\nTries: ");
-    print_int(tries);
-    print("\nGuess letter: ");
-    char c = get_key();
-    put_char(c);
-    if (c >= 'a' && c <= 'z')
-      c -= 32; // To upper
-
-    bool found = false;
-    for (int i = 0; i < 6; i++)
-      if (word[i] == c) {
-        guess[i] = c;
-        found = true;
-      }
-    if (!found)
-      tries--;
-  }
-  if (tries > 0)
-    print("\nYOU WIN!\n");
-  else
-    print("\nGAME OVER\n");
-}
-
-/* ============= UTILS COMMANDS ============= */
-void cmd_matrix() {
-  clear_screen();
-  for (int i = 0; i < 500; i++) {
-    put_char_at(rand() % 80, rand() % 25, rand() % 2 ? '1' : '0', LIGHT_GREEN,
-                BLACK);
-    sleep(10);
-    if (check_key())
-      break;
-  }
-  clear_screen();
-}
-
-void cmd_help() {
-  print_color("\n=== EXTENDED COMMAND LIST ===\n", YELLOW);
-  print("Files: ls, cat, touch, mkdir, rm, cp, mv\n");
-  print("System: date, time, uptime, whoami, uname, hostname, ps, top, free, "
-        "df\n");
-  print("Utils: clear, echo, calc, wc, grep, head, tail, history, neofetch\n");
-  print("Games: snake, tetris, hangman, 2048, tictactoe\n");
-  print("Fun: matrix, fortune, cowsay, banner\n");
-  print("Easter Eggs: sudo, make, reboot\n");
-}
-
-void cmd_neofetch() {
-  print_color("\n       /\\        OS: TarkOS v1.0\n", CYAN);
-  print_color("      /  \\       Kernel: Micro-monolithic\n", CYAN);
-  print_color("     /    \\      Shell: TarkSH v1.0\n", CYAN);
-  print_color("    /______\\     Res: 80x25 VGA\n", CYAN);
-  print_color("   /        \\    CPU: i386 Protected Mode\n", CYAN);
-  print_color("  /__________\\   RAM: 128 MB\n", CYAN);
-}
-
-/* ============= SHELL LOOP ============= */
-void kmain(void) {
   fs_init();
   set_color(WHITE, BLUE);
   clear_screen();
@@ -527,22 +542,19 @@ void kmain(void) {
   // Header
   for (int x = 0; x < 80; x++)
     put_char_at(x, 0, ' ', WHITE, CYAN);
-  print_at(2, 0, "TarkOS v1.0 - The Ultimate Hobby OS", WHITE, CYAN);
-  print_at(70, 0, "USER: root", YELLOW, CYAN);
+  print_at(2, 0, "TarkOS v1.0 - Real Hardware", WHITE, CYAN);
 
-  // Welcome
   cursor_y = 2;
   print_color("Welcome to TarkOS v1.0!\n", LIGHT_GREEN);
-  print("Type 'help' for the list of 50+ commands.\n\n");
+
+  // Auto-detect hardware
+  cmd_info(mboot);
 
   char cmd[64];
   while (1) {
-    print_color("root@tarkos", LIGHT_GREEN);
-    print(":");
-    print_color("/", LIGHT_BLUE);
-    print("$ ");
+    print_color("\nroot@tarkos", LIGHT_GREEN);
+    print(":$ ");
 
-    // Read line
     int pos = 0;
     memset(cmd, 0, 64);
     while (1) {
@@ -563,60 +575,26 @@ void kmain(void) {
       }
     }
 
-    // Execute
     if (strlen(cmd) == 0)
       continue;
     else if (strcmp(cmd, "help") == 0)
-      cmd_help();
+      print("\nCommands: ls, cat, info, date, snake, clear, reboot\n");
     else if (strcmp(cmd, "ls") == 0)
       cmd_ls();
-    else if (strncmp(cmd, "cat ", 4) == 0)
-      cmd_cat(cmd + 4);
-    else if (strncmp(cmd, "touch ", 6) == 0)
-      cmd_touch(cmd + 6);
-    else if (strncmp(cmd, "mkdir ", 6) == 0)
-      cmd_mkdir(cmd + 6);
-    else if (strncmp(cmd, "rm ", 3) == 0)
-      cmd_rm(cmd + 3);
+    else if (strcmp(cmd, "info") == 0)
+      cmd_info(mboot);
+    else if (strcmp(cmd, "date") == 0)
+      cmd_date();
+    else if (strcmp(cmd, "snake") == 0)
+      game_snake();
     else if (strcmp(cmd, "clear") == 0) {
       clear_screen();
       cursor_y = 2;
-    } else if (strcmp(cmd, "snake") == 0)
-      game_snake();
-    else if (strcmp(cmd, "tetris") == 0)
-      game_tetris();
-    else if (strcmp(cmd, "hangman") == 0)
-      game_hangman();
-    else if (strcmp(cmd, "matrix") == 0)
-      cmd_matrix();
-    else if (strcmp(cmd, "neofetch") == 0)
-      cmd_neofetch();
-    else if (strcmp(cmd, "uptime") == 0) {
-      print("Uptime: ");
-      print_int(ticks / 18);
-      print(" sec\n");
-    } else if (strcmp(cmd, "whoami") == 0)
-      print("root\n");
-    else if (strcmp(cmd, "date") == 0)
-      print("Thu Jan 29 2026\n"); // Fake date
-    else if (strcmp(cmd, "pwd") == 0)
-      print("/\n");
-    else if (strcmp(cmd, "reboot") == 0) {
-      print("Rebooting...\n");
+    } else if (strcmp(cmd, "reboot") == 0)
       outb(0x64, 0xFE);
-    } else if (strncmp(cmd, "echo ", 5) == 0) {
-      print(cmd + 5);
-      print("\n");
-    } else if (strcmp(cmd, "cowsay") == 0)
-      print(" ^__^\n (oo)\n (__)  Moo!\n");
-    else if (strcmp(cmd, "fortune") == 0)
-      print("You will write an OS today.\n");
-    else if (strcmp(cmd, "exit") == 0)
-      print("No escape.\n");
-    else {
-      print("Unknown command: ");
-      print(cmd);
-      print("\n");
-    }
+    else if (strncmp(cmd, "cat ", 4) == 0)
+      cmd_cat(cmd + 4);
+    else
+      print("\nUnknown command.\n");
   }
 }
